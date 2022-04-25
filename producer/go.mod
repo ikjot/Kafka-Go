a48517ce8526cb7560a5b3b56461742c1b4cf341
ikkaur <ikkaur@cisco.com>
2022-04-25 09:13:33 -0700

    clean up and use environment variables

--- producer/go.mod
+++ producer/go.mod
@@ -2,13 +2,9 @@ module producer
 
 go 1.18
 
-require (
-	github.com/Shopify/sarama v1.32.0
-	github.com/gofiber/fiber/v2 v2.32.0
-)
+require github.com/Shopify/sarama v1.32.0
 
 require (
-	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/eapache/go-resiliency v1.2.0 // indirect
 	github.com/eapache/go-xerial-snappy v0.0.0-20180814174437-776d5712da21 // indirect
@@ -23,10 +19,6 @@ require (
 	github.com/klauspost/compress v1.15.0 // indirect
 	github.com/pierrec/lz4 v2.6.1+incompatible // indirect
 	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.35.0 // indirect
-	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
 	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
-	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
 )
--- producer/producer.go
+++ producer/producer.go
@@ -3,18 +3,22 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/Shopify/sarama"
 )
 
 func main() {
-	cmt := 123456
-	cmtInBytes, err := json.Marshal(cmt)
+
+	cmtInBytes, err := json.Marshal("***-Dummy-value-***")
 	if err != nil {
 		fmt.Println("Error while json marshalling")
 	}
+	var brokersUrl []string = strings.Split(os.Getenv("BROKERS_URL"), ",")
+	topic := os.Getenv("TOPIC_NAME")
 
-	error := PushCommentToQueue("comments", cmtInBytes)
+	error := PushCommentToQueue(brokersUrl, topic, cmtInBytes)
 	if error != nil {
 		fmt.Println("Error while pushing comment to Queue")
 	} else {
@@ -37,12 +41,12 @@ func ConnectProducer(brokersUrl []string) (sarama.SyncProducer, error) {
 	return conn, nil
 }
 
-func PushCommentToQueue(topic string, message []byte) error {
+func PushCommentToQueue(brokersUrl []string, topic string, message []byte) error {
 
-	brokersUrl := []string{"127.0.0.1:9092"}
 	producer, err := ConnectProducer(brokersUrl)
 	if err != nil {
-		fmt.Println("Error while ConnectProducer")
+		fmt.Println("Error while connecting producer")
+		return err
 	}
 
 	defer producer.Close()
@@ -52,19 +56,14 @@ func PushCommentToQueue(topic string, message []byte) error {
 		Value: sarama.StringEncoder(message),
 	}
 
-	fmt.Println(msg.Topic)
-	fmt.Println(msg.Value)
-	fmt.Println(msg.Timestamp)
-
 	partition, offset, err := producer.SendMessage(msg)
 	if err != nil {
-		fmt.Println("Error while SendMessage")
+		fmt.Println("Error while sending message to Kafka")
 		fmt.Println(err.Error())
 
 		return err
 	}
 
-	fmt.Printf("Message is stored in topic(%s)/partition(%d)/offset(%d)\n", topic, partition, offset)
-
+	fmt.Printf("Success. Message is stored in topic(%s)/partition(%d)/offset(%d)\n", topic, partition, offset)
 	return nil
 }
